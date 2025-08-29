5849cb7a148496522858a11b3c4c3e66dca995a4
vatsaljain-pdeu <vatsalnjain19@gmail.com>
2025-08-29 11:54:15 +0530

    Create rupees to dollars.c

---
+++ lab1/rupees to dollars.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+ int main()
+ {
+ float rupees, dollars;
+ printf("Enter amount in rupees: " );
+ scanf("%f", &rupees);
+ dollars = rupees / 84;
+ printf("Dollars = %.2f\n", dollars);
+ return 0;
+ }
