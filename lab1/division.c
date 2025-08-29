45eb8d17f31a1525a98e6c110de694b42175e215
vatsaljain-pdeu <vatsalnjain19@gmail.com>
2025-08-29 11:47:54 +0530

    Create division.c

---
+++ lab1/division.c
@@ -0,0 +1,9 @@
+ #include <stdio.h>
+ int main() {
+ float a, b, quotient;
+ printf("Enter two numbers: " );
+ scanf("%f %f", &a, &b);
+ quotient = a / b;
+ printf("Quotient = %.2f\n", quotient);
+ return 0;
+ }
