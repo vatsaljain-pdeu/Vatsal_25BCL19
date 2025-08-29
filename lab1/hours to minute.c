b2d23a45e1485f8071574efa83e634bc8a42b1fb
vatsaljain-pdeu <vatsalnjain19@gmail.com>
2025-08-29 11:50:48 +0530

    Create hours to minute.c

---
+++ lab1/hours to minute.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+ int main()
+ {
+ int hours, minutes;
+ printf("Enter hours: " );
+ scanf("%d", &hours);
+ minutes = hours * 60;
+ printf("Minutes = %d\n", minutes);
+ return 0;
+ }
